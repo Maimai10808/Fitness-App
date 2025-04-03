8b66c85574db46b6d0a83621b506e9193cd47d28
Maimai10808 <331686549@qq.com>
2025-04-03 15:53:33 +0800

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by mac on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
